deb752e9a12d23802f2d551120120e7d542d7ad6
François Nsengimana <francknsengimana@gmail.com>
2025-06-03 20:42:18 +0100

    ajout du champs de saisi et remplissage des informations sur formulaire des données des etudiants

--- GestionEtudiants.py
+++ GestionEtudiants.py
@@ -23,10 +23,43 @@ class GestionEtudiants:
         manageFrame = Frame(self.root, bd=2, relief="groove", bg='#1E02F2')
         manageFrame.place(x=20, y=130, width=0.32 * largeur_ecran, height=500)
 
-        titleLabel = Label(manageFrame, text="Information sur l'etudiant",font=('ubuntu', 18, 'bold'),bg='blue',fg='white' )
+        titleLabel = Label(manageFrame, text="Information sur l'etudiant",font=('ubuntu', 18, 'bold'),bg='#1E02F2',fg='white' )
         titleLabel.grid(row=0, columnspan=2, pady=15)
 
-        # ========ajout du bouton d'action de etudiants  des etudiants =======================
+        # ========Champs de remplisage des infos sur formulaire de saisi des données des etudiants =======================
+        ineLabelEtudiant = Label(manageFrame, text="INE:",font=('ubuntu', 12, 'bold'),bg='#1E02F2',fg='white')
+        ineLabelEtudiant.grid(row=1, column=0, pady=10, sticky='w')
+        ineLabelEtudiantText = Entry(manageFrame,font=('Times new roman', 12, 'bold'),bd=2,relief="groove", width=30)
+        ineLabelEtudiantText.grid(row=1, column=1, padx=10, pady=10, sticky='w' )
+
+        nomLabelEtudiant = Label(manageFrame, text="Nom:",font=('ubuntu', 12, 'bold'),bg='#1E02F2',fg='white')
+        nomLabelEtudiant.grid(row=2, column=0, pady=10, sticky='w')
+        nomLabelEtudiantText = Entry(manageFrame,font=('Times new roman', 12, 'bold'),bd=2,relief="groove", width=30)
+        nomLabelEtudiantText.grid(row=2, column=1, padx=10, pady=10, sticky='w' )
+
+        prenomLabelEtudiant = Label(manageFrame, text="Prénom:", font=('ubuntu', 12, 'bold'), bg='#1E02F2', fg='white')
+        prenomLabelEtudiant.grid(row=3, column=0, pady=10, sticky='w')
+        prenomLabelEtudiantText = Entry(manageFrame, font=('Times new roman', 12, 'bold'), bd=2, relief="groove", width=30)
+        prenomLabelEtudiantText.grid(row=3, column=1, padx=10, pady=10, sticky='w')
+
+        emailLabelEtudiant = Label(manageFrame, text="Adresse email:", font=('ubuntu', 12, 'bold'), bg='#1E02F2', fg='white')
+        emailLabelEtudiant.grid(row=4, column=0, pady=10, sticky='w')
+        emailLabelEtudiantText = Entry(manageFrame, font=('Times new roman', 12, 'bold'), bd=2, relief="groove", width=30)
+        emailLabelEtudiantText.grid(row=4, column=1, padx=10, pady=10, sticky='w')
+
+        adresseLabelEtudiant = Label(manageFrame, text="Adresse:", font=('ubuntu', 12, 'bold'), bg='#1E02F2', fg='white')
+        adresseLabelEtudiant.grid(row=5, column=0, pady=10, sticky='w')
+        adresseLabelEtudiantText = Entry(manageFrame, font=('Times new roman', 12, 'bold'), bd=2, relief="groove", width=30)
+        adresseLabelEtudiantText.grid(row=5, column=1, padx=10, pady=10, sticky='w')
+
+        villeLabelEtudiant = Label(manageFrame, text="Ville:", font=('ubuntu', 12, 'bold'), bg='#1E02F2', fg='white')
+        villeLabelEtudiant.grid(row=6, column=0, pady=10, sticky='w')
+        villeLabelEtudiantText = Entry(manageFrame, font=('Times new roman', 12, 'bold'), bd=2, relief="groove", width=30)
+        villeLabelEtudiantText.grid(row=6, column=1, padx=10, pady=10, sticky='w')
+
+
+
+        # ========ajout du bouton d'action de etudiants  des etudiants =================
         boutonFrame = Button(manageFrame, bd=2, relief="groove", bg='#1E02F2')
         boutonFrame.place(x=20, y=400, width=0.28  * largeur_ecran, height=70)
 
